1c580ab0a3e4ec638ea61a6f63d883793aeb9f11
PRABHAKAR KUMAR SINGH <prabhakarks90@hotmail.com>
2022-03-17 14:48:51 +0530

    ok

--- frmImportData.py
+++ frmImportData.py
@@ -1,7 +1,16 @@
 import PyPDF2 as pdf
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import StringIO
+import io
+
 import tkinter as tk
 from tkinter.filedialog import askopenfile, askopenfilename
 import GenerateConfig as Gc
+
+
 
 class ImportData(tk.Frame):
     config=None
@@ -14,17 +23,55 @@ class ImportData(tk.Frame):
         self.pack(expand=True, fill=tk.BOTH)
         self.fncCreateItems()
     
+    def convert_pdf_to_txt(self,path):
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = open(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos=set()
+
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+            interpreter.process_page(page)
+
+        text = retstr.getvalue()
+
+        fp.close()
+        device.close()
+        retstr.close()
+        return text
+    def open_file1(self):
+        open_file = askopenfilename(initialdir="d:",title="Open Template" , filetypes =[('Pdf Files', '*.pdf')])
+        if open_file: 
+            txtbox=self.children["txtremarks"]
+            txtbox.delete(1.0,"end")             
+            txtbox.insert("end",self.convert_pdf_to_txt(open_file))
+            
 
     def open_file(self):
         open_file = askopenfilename(initialdir="d:",title="Open Template" , filetypes =[('Pdf Files', '*.pdf')])
         if open_file:            
-            pdfReader = pdf.PdfFileReader(open_file)
-            #content = pdfReader.read()
-            print(pdfReader.numPages)
-            # creating a page object
-            pageObj = pdfReader.getPage(0)
-            # extracting text from page
-            print(pageObj.extractText())
+            pdfReader = pdf.PdfFileReader(open_file,'rb')            
+            totalPages=pdfReader.numPages
+            currentPages=0
+            txtbox=self.children["txtremarks"]
+            txtbox.delete(1.0,"end")
+            while (currentPages<totalPages):
+                # creating a page object
+                pageObj = pdfReader.getPage(currentPages)
+                # extracting text from page
+                pageData=pageObj.extractText()
+                #pageData=pageObj.getContents()
+                txtbox.insert("end",pageData)
+                currentPages=currentPages+1;
+            
+                
+
             
             # closing the pdf file object
             #pdfFileObj.close()
@@ -44,15 +91,16 @@ class ImportData(tk.Frame):
 
         
         btnImport = tk.Button ( self, text ="Import",width=10, relief='flat', font=self.config.displayFont,fg=self.config.COLOR_MENU_BACKGROUND,bg=self.config.COLOR_TOP_BACKGROUND,  command =lambda:self.open_file() )
-        btnSave = tk.Button ( self, text ="Save", width=10,relief='flat', font=self.config.displayFont,fg=self.config.COLOR_MENU_BACKGROUND,bg=self.config.COLOR_TOP_BACKGROUND, command =lambda: self.save_data())
+        btnSave = tk.Button ( self, text ="Save", width=10,relief='flat', font=self.config.displayFont,fg=self.config.COLOR_MENU_BACKGROUND,bg=self.config.COLOR_TOP_BACKGROUND, command =lambda: self.open_file1())
         btnImport.bind('<Enter>', self.config.on_enter_button)
         btnImport.bind('<Leave>', self.config.on_leave_button)
         btnSave.bind('<Enter>', self.config.on_enter_button)
         btnSave.bind('<Leave>', self.config.on_leave_button)
         btnImport.place(x = 170,y = 10+yaxis, anchor=tk.NW)
         btnSave.place(x = 270,y = 10+yaxis, anchor=tk.NW)
-        txtDetail=tk.Text(self,height=30)
-        txtDetail.pack(expand=True, )
+        yaxis=yaxis+50
+        txtDetail=tk.Text(self,height=5, name="txtremarks")
+        txtDetail.place(x = 170,y = 10+yaxis, anchor=tk.NW)
         
         
 if __name__ == '__main__':
