1f608408858e712508d1f584300da8f413f21d93
PRABHAKAR KUMAR SINGH <prabhakarks90@hotmail.com>
2022-05-20 17:19:33 +0530

    Delete unnecessary File

--- GenerateConfig.py
+++ GenerateConfig.py
@@ -263,6 +263,8 @@ class GenerateConfig:
         wget.download(url, out=dirpath)
         url="http://wms.galway.in/afill/data.json"
         wget.download(url, out=dirpath)
+        url="http://wms.galway.in/afill/PankajAggarwal.json"
+        wget.download(url, out=dirpath)
         url="http://wms.galway.in/afill/logo.png"
         wget.download(url, out=filepath)
         url="http://wms.galway.in/afill/logoicon.png"
